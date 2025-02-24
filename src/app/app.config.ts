12d8c96cca2b0e58ee7197dacd2671d7b6a7fb0b
YueYuuta <jcjunior308@gmail.com>
2025-02-23 23:35:27 -0500

    add(devtool) redux

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
 import {
   ApplicationConfig,
   importProvidersFrom,
+  isDevMode,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
@@ -19,6 +20,7 @@ import { HttpErrorInterceptor } from './interceptors/http-error.interceptor';
 import { PRODUCT_STORE } from './services/product.store.interface';
 import { ProductNgrxService } from './services/product.ngrx.service';
 import { ProductSignalService } from './services/product.signal.service';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -30,5 +32,13 @@ export const appConfig: ApplicationConfig = {
     { provide: HTTP_INTERCEPTORS, useClass: HttpErrorInterceptor, multi: true },
     provideStore(reducers, { metaReducers }),
     provideEffects(ProductEffects),
+    provideStoreDevtools({
+      maxAge: 25, // Retains last 25 states
+      logOnly: !isDevMode(), // Restrict extension to log-only mode
+      // autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+      // trace: false, //  If set to true, will include stack trace for every dispatched action, so you can see it in trace tab jumping directly to that part of code
+      // traceLimit: 75, // maximum stack trace frames to be stored (in case trace option was provided as true)
+      // connectInZone: true, // If set to true, the connection is established within the Angular zone
+    }),
   ],
 };
